6db1e48f3612029f758af1401fb15117b9687987
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2024-12-30 16:23:48 +0530

    Add component files

---
+++ youtube-playlist/Components/MainContent.jsx
@@ -0,0 +1,4 @@
+import Video from "../Components/video";
+export default function MainContent() {
+  return <Video />;
+}
---
+++ youtube-playlist/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+import Sidebar from "../Components/Sidebar";
+import MainContent from "../Components/MainContent";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <MainContent />
+    </>
+  );
+}
