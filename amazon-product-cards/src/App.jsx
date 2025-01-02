99c9b31d61048f546622e9f32de5e606d013ed28
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2025-01-02 12:41:12 +0530

    Updated assets and file structures

---
+++ amazon-product-cards/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  );
+}
