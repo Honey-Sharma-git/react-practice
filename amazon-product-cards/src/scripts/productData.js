d1dc4593243eaf72509cee36231f6039a6b5f762
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2025-01-02 12:19:18 +0530

    Initial commit

---
+++ amazon-product-cards/src/scripts/productData.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    id: 1,
+    image: {
+      src: "src/assets/images/earphones.jpg",
+      alt: "branded earphones",
+    },
+    title: "Oneplus Earphones",
+    ratings: {
+      stars: "src/assets/ratings/rating-0.png",
+      count: 21,
+    },
+    price: 1299,
+  },
+];
