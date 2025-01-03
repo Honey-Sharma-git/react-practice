185be972668c818ba78cc968cd2ebcbba7710447
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2025-01-03 15:33:15 +0530

    Add increment and decrement functions

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,10 +1,26 @@
+import React from "react";
 export default function App() {
+  const [count, setCount] = React.useState(0);
+  function add() {
+    setCount((prevValue) => {
+      return prevValue + 1;
+    });
+  }
+  function subtract() {
+    setCount((prevValue) => {
+      return prevValue - 1;
+    });
+  }
   return (
     <main className="main">
-      <p className="main__para">0</p>
+      <p className="main__para">{count}</p>
       <div className="btn-container">
-        <button className="btn">-</button>
-        <button className="btn">+</button>
+        <button className="btn" onClick={subtract}>
+          -
+        </button>
+        <button className="btn" onClick={add}>
+          +
+        </button>
       </div>
     </main>
   );
