f2bd9a585134361c4f29ab2002f5df54c978a187
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2025-01-10 15:57:45 +0530

    change in contact card

--- contact-card-fav/components/Contact.jsx
+++ contact-card-fav/components/Contact.jsx
@@ -1,27 +1,59 @@
-export default function Contact() {
+import React from "react";
+export default function Contact(props) {
+  //De-structured props to supply data:
+  const { firstName, lastName, mobile, email, about, src, alt } = props;
+
+  //  React state for toggling favorite
+  const [isFavorite, setIsFavorite] = React.useState(false);
+
+  //Updating state on clicking star button
+  function toggleFavorite() {
+    setIsFavorite((prev) => {
+      return !prev;
+    });
+  }
+
+  //File path for star-icon:
+  const favorite = {
+    filled: { src: "/src/assets/image/star-filled.png", alt: "Favorite" },
+    empty: { src: "/src/assets/image/star-empty.png", alt: "Not favorite" },
+  };
+
   return (
     <article className="contact-card">
       <figure className="pic-container">
-        <img className="pic" src="/src/assets/image/user.png" alt="" />
+        <img className="pic" src={src} alt={alt} />
       </figure>
-      <figure className="star-container">
-        <img className="star" src="/src/assets/image/star-empty.png" alt="" />
+
+      <figure
+        className="star-container"
+        //Function to toggle state
+        onClick={toggleFavorite}
+      >
+        <img
+          className="star"
+          //   Changing star-icon image according to boolean
+          src={isFavorite ? favorite.filled.src : favorite.empty.src}
+          alt={isFavorite ? favorite.filled.alt : favorite.empty.alt}
+        />
       </figure>
-      <h1 className="contact-name">Honey Sharma</h1>
+      <h1 className="contact-name">
+        {firstName} {lastName}
+      </h1>
 
       <p className="contact-mobile details-container">
-        <span className="title">Mobile:</span> <span className="data">924293424</span>
+        <span className="title">Mobile:</span>
+        <span className="data">{mobile}</span>
       </p>
 
       <p className="contact-email details-container">
-        <span className="title">Email:</span> <span className="data">aldfhasldfh@gmail.com</span>
+        <span className="title">Email:</span>
+        <span className="data">{email}</span>
       </p>
 
       <p className="contact-about details-container">
         <span className="title">About:</span>
-        <span className="data">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Volupta
-        </span>
+        <span className="data">{about}</span>
       </p>
     </article>
   );
---
+++ contact-card-fav/src/scripts/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    firstName: "honey",
+    lastName: "Sharma",
+    mobile: "8955687845",
+    email: "honey@example.com",
+    about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Volupta",
+    image: {
+      src: "/src/assets/image/user.png",
+      alt: "Profile picture",
+    },
+  },
+  {
+    id: 2,
+    firstName: "Pulkit",
+    lastName: "Pareek",
+    mobile: "9955687845",
+    email: "pulkit@example.com",
+    about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Volupta",
+    image: {
+      src: "/src/assets/image/user.png",
+      alt: "Profile picture",
+    },
+  },
+];
