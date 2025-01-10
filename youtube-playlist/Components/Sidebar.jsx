75b6c425f1a4f6c69f1e9191be8f0743755b32ed
Honey-Sharma-git <Honeysharmaemail@gmail.com>
2025-01-10 16:03:58 +0530

    adding in main

--- youtube-playlist/Components/Sidebar.jsx
+++ youtube-playlist/Components/Sidebar.jsx
@@ -1,3 +1,24 @@
 export default function Sidebar() {
-  return <p>sidebar</p>;
+  return (
+    <article>
+      <button>
+        <img src="./src/assets/icons/menu-icon.svg" alt="Menu icon" />
+      </button>
+      <button>
+        <img src="./src/assets/icons/home-icon.svg" alt="home icon" />
+      </button>
+      <button>
+        <img src="./src/assets/icons/shorts.svg" alt="shorts icons" />
+      </button>
+      <button>
+        <img
+          src="./src/assets/icons/subscription.png"
+          alt="subscription icon"
+        />
+      </button>
+      <button>
+        <img src="./src/assets/icons/user.svg" alt="user icon" />
+      </button>
+    </article>
+  );
 }
